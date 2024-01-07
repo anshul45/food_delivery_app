d044ead1198e1f2092eaa8e7d9ebf93f0adfd4c8
Anshul45 <anshulgarwal025@gmail.com>
2024-01-07 23:31:17 +0530

    frontend part complete

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,39 +1,50 @@
-import Image from "next/image"
-import image from "../../assets/product_01.jpg"
+import Image from "next/image";
+import productimage from "../../assets/product_01.jpg";
 export default function Cart() {
-    return <div>
-        <div className=" bg-green-400 w-full py-20 pl-32 font-semibold text-4xl text-white">Your Cart</div>
-       <div className="mx-32 my-12">
-       {true?( <>    <div className="flex justify-between">
-                <h1>Image</h1>
-                <h1>Product Title</h1>
-                <h1>Price</h1>
-                <h1>Quantity</h1>
-                <h1>Delete</h1>
+  return (
+    <div>
+      <div className=" bg-green-400 w-full py-20 pl-32 font-semibold text-4xl text-white">
+        Your Cart
+      </div>
+      <div className="mx-32 my-12">
+        {true ? (
+          <>
+            {" "}
+            <div className="flex justify-between">
+              <h1>Image</h1>
+              <h1>Product Title</h1>
+              <h1>Price</h1>
+              <h1>Quantity</h1>
+              <h1>Delete</h1>
             </div>
             <div className="flex justify-between my-8">
-                <Image alt="product" width={30} height={30} src={image}  />
-                <h1>Product Title</h1>
-                <h1>Price</h1>
-                <h1>Quantity</h1>
-                <h1>Delete</h1>
+              <Image alt="product" width={30} height={30} src={productimage} />
+              <h1>Product Title</h1>
+              <h1>Price</h1>
+              <h1>Quantity</h1>
+              <h1>Delete</h1>
             </div>
-            </>
-            ):
-            <div className="flex justify-center my-9 font-semibold text-2xl">
-                Your cart is empty</div>}
+          </>
+        ) : (
+          <div className="flex justify-center my-9 font-semibold text-2xl">
+            Your cart is empty
+          </div>
+        )}
 
-            <div className='flex gap-3 items-center'>
-                <h3 className='font-semibold text-lg'>Subtotal:</h3>
-                <h1 className='font-semibold text-2xl text-[#df2020]'>0</h1>
-              </div>
+        <div className="flex gap-3 items-center">
+          <h3 className="font-semibold text-lg">Subtotal:</h3>
+          <h1 className="font-semibold text-2xl text-[#df2020]">0</h1>
+        </div>
 
-              <div className="flex mt-8 gap-12">
-                <button className="bg-[#df2020] font-semibold text-lg text-white px-4 py-1 rounded-md">Continue Shoppping</button>
-                <button className="bg-[#df2020] font-semibold text-lg text-white px-4 py-1 rounded-md">Proceed to checkout</button>
-              </div>
-              </div>
-
-        
+        <div className="flex mt-8 gap-12">
+          <button className="bg-[#df2020] font-semibold text-lg text-white px-4 py-1 rounded-md">
+            Continue Shoppping
+          </button>
+          <button className="bg-[#df2020] font-semibold text-lg text-white px-4 py-1 rounded-md">
+            Proceed to checkout
+          </button>
+        </div>
+      </div>
     </div>
-} 
+  );
+}
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import user1 from "../assets/ava-2.jpg";
+import user2 from "../assets/ava-3.jpg";
+import user3 from "../assets/ava-4.jpg";
 
 const Carousel = () => {
+  const [index, setIndex] = useState(0);
+  const users = [
+    {
+      id: 0,
+      name: "Jhon Doe",
+      desc: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, delectus temporibus. Placeat quo id animi dolorem. Blanditiis, deleniti. Nulla officiis omnis ex quibusdam atque facilis minus quam suscipit id ",
+      img: user2,
+    },
+    {
+      id: 1,
+      name: "Mitchel Marsh",
+      desc: "Consectetur earum at cupiditate rem error dolore, fugiat facilis sed dicta. Aut explicabo saepe aliquam quidem doloribus quae tenetur, quas adipisci dicta fuga aliquid debitis quasi quo? Officia, tenetur rerum!",
+      img: user1,
+    },
+    {
+      id: 2,
+      name: "Steven Crock",
+      desc: "  Sequi nisi magni quos, quia expedita fugiat quisquam quidem aliquam impedit officia odio minima corrupti quam facilis eum quis illo placeat voluptas minus voluptates quibusdam. Molestias tenetur tempore quia similique.",
+      img: user3,
+    },
+  ];
+
+  const handleClick = (newIndex: number) => {
+    setIndex(newIndex);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const nextIndex = (index + 1) % users.length;
+      setIndex(nextIndex);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [index, users.length]);
+
   return (
-    <div>
-      <h1>Testimonial</h1>
+    <div className="mt-14 relative overflow-hidden">
+      <p className="leading-7 text-gray-400">{users[index].desc}</p>
+      <div className="flex items-center gap-5 mt-8">
+        <Image
+          alt="user"
+          src={users[index].img}
+          width={50}
+          height={50}
+          className="rounded-md object-fill"
+        />
+        <h1 className="font-semibold text-xl">{users[index].name}</h1>
+      </div>
+      <div className="relative bottom-0 left-56">
+        {[0, 1, 2].map((idx) => (
+          <i
+            key={idx}
+            className={` text-xs mx-3 cursor-pointer ${
+              idx === index ? "ri-circle-fill" : "ri-circle-line"
+            }`}
+            onClick={() => handleClick(idx)}
+          ></i>
+        ))}
+      </div>
     </div>
   );
 };
